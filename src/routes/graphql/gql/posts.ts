ce8691c74f7f3ccd6f1bb2cd69375e436f9e098a
sergereinov <47950629+sergereinov@users.noreply.github.com>
2023-01-30 21:20:07 +0300

    feat: add posts gql

---
+++ src/routes/graphql/gql/posts.ts
@@ -0,0 +1,31 @@
+import { GraphQLError } from 'graphql/error';
+import { DBApi } from '../../../utils/DB/DBApi';
+import { PostEntity, CreatePostDTO } from '../../../utils/DB/entities/DBPosts';
+import * as usersResolver from './users';
+
+/**
+ * Posts resolvers and wrappers
+ */
+
+/**
+ * Wrap post object to match GraphQL schema `type Post`
+ */
+export const wrapPost = (post: PostEntity) => ({
+    ...post,
+    user: async ({ }, adb: DBApi) => usersResolver.user(post.userId, adb)
+});
+
+export const posts = async (adb: DBApi) =>
+    (await adb.posts.getAll()).map((e) => wrapPost(e));
+
+export const postsByUserId = async (userId: string, adb: DBApi) =>
+    (await adb.posts.getByUserId(userId)).map((e) => wrapPost(e))
+
+export const post = async (postId: string, adb: DBApi) => {
+    const post = await adb.posts.getById(postId);
+    if (!post) return new GraphQLError('post not found');
+    return wrapPost(post);
+}
+
+export const createPost = async (dto: CreatePostDTO, adb: DBApi) =>
+    wrapPost(await adb.posts.create(dto));
